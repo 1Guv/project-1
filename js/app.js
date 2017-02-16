9f5a14309dbccac76f8d091b887482d64ee8f7e2
Guv <Gurvinder.Singh.Sandhu@gmail.com>
2017-02-16 21:02:28 +0000

    aligned the pic in the credits page

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 $(() => {
 
-  // var awc = awc || {};
-
   //VARIABLES/////////////////////////////////////
   const $screenWidth = $(window).width();     // gets the current window WIDTH
   const $screenHeight = $(window).height();   // gets the current window HEIGHT
